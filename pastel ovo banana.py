bbc20455f4892a9198014b70568846204ed1dfe0
André <andrefernandesoares@gmail.com>
2024-08-30 10:47:04 -0300

    pastel ovo banana 2.0.py

--- pastel ovo banana.py
+++ pastel ovo banana.py
@@ -1,20 +1,27 @@
+# Vai dar pau se não instalar o PySimpleGui
+
+#Para instalar coloque no CMD 'python -m pip install pysimplegui'
+
+
+
+
 import PySimpleGUI as sg
 b = ['pastel','ovo','banana']
 a = 0
 
-# All the stuff inside your window.
+
 layout = [  [sg.Text(f'Vou comer seu {b[a]}', key='a')],
             [sg.Button('?'), sg.Button('EXPLODIR')] ]
 
-# Create the Window
+
 window = sg.Window('Pastel, ovo, banana', layout)
 
-# Event Loop to process "events" and get the "values" of the inputs
+
 while True:
     event, values = window.read()
     
     
-    # if user closes window or clicks cancel
+    
     if event == sg.WIN_CLOSED or event == 'EXPLODIR':
 
         break
