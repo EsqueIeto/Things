aa5d76d3113e77320b1290f16740c64f0e12842c
André <andrefernandesoares@gmail.com>
2024-09-24 19:34:36 -0300

    case.py

--- case.py
+++ case.py
@@ -1,4 +1,4 @@
-import os as a
+import os as c
 import time as b
 
 a = ['/p','Sistem','Drag','Time','/f','Time of date','shutdown','Version','Windows','Lua','C#']
@@ -9,4 +9,4 @@ match str(input('Sim ou Não?\n')).capitalize():
     case 'Não':
         print('Morra >:c')
         b.sleep(1)
-        a.system(f'{a[6]} {a[0]} {a[4]}')
+        c.system(f'{a[6]} {a[0]} {a[4]}')
